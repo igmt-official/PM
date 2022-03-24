258cf48cf2cfcf3268efa49b29bc3d85145cd470
igmt-official <igmtofficial@gmail.com>
2022-03-25 01:48:59 +0800

    Updated.

--- ui.py
+++ ui.py
@@ -1,151 +1,120 @@
-import os
 import sys
 import json
 import string
 import random
 
-# PyQt5 Classes
+# PyQt5 Class
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets, QtGui
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLineEdit
 
 
+
 # Load Ui
 class Ui(QMainWindow):
-        def __init__(self):
-                super(Ui, self).__init__()
-                loadUi("ui.ui", self)
+    def __init__(self):
+        super(Ui, self).__init__()
+        loadUi("ui.ui", self)
 
-                # Button
-                self.generateButton.clicked.connect(self.generateRandomPassword)
-                self.saveButton.clicked.connect(self.save)
-                self.searchButton.clicked.connect(self.search)
-                self.passwordEchoType.clicked.connect(self.passwordEcho)
+        self.generateButton.clicked.connect(self.randomPassword)
+        self.saveButton.clicked.connect(self.save)
+        self.searchButton.clicked.connect(self.search)
+        self.passwordEchoType.clicked.connect(self.passwordEcho)
 
-                self.show()
+        self.show()
 
-        # Password Generator Button
-        def generateRandomPassword(self):
-                lower = string.ascii_lowercase
-                upper = string.ascii_uppercase
-                numbers = string.digits
-                symbols = string.punctuation
+    # Search
+    def search(self):
+        web = self.webLine.text()
 
-                length = 8
-                temp = lower + upper + numbers + symbols
+        if web == "":
+            self.alert("warning", "Please fill out the website field.")
+            return
 
-                random_password = random.sample(temp, length)
-                password = "".join(random_password)
+        try:
+            with open("./data.json", "r") as data:
+                data_file = json.load(data)
+        except FileNotFoundError:
+            self.alert("warning", "No data found.")
+        else:
+            if web not in data_file:
+                self.alert("warning", f"{web} is not exist.")
+            else:
+                self.emailLine.setText(data_file[web]["email"])
+                self.passwordLine.setText(data_file[web]["password"])
 
-                self.passwordLine.setText(password)
+    # Generate Random Password
+    def randomPassword(self):
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase
+        num = string.digits
+        sym = string.punctuation
 
-                # Debug
-                # print(data.password)
-                # print(password)
+        length = 8
+        temp = lower + upper + num + sym
 
-        # Save Button
-        def save(self):
-                web = self.websiteLine.text()
-                email = self.emailLine.text()
-                password = self.passwordLine.text()
+        random_password = random.sample(temp, length)
+        password = "".join(random_password)
 
-                if web == "" or email == "" or password == "":
-                        self.alert("warning", "Please fill out the blank.")
-                        return
+        self.passwordLine.setText(password)
 
-                new_data = {
-                        web: {
-                                "email": email,
-                                "password": password
-                        }
-                }
+    # Save
+    def save(self):
+        web = self.webLine.text()
+        email = self.emailLine.text()
+        password = self.passwordLine.text()
 
-                try:
-                        # Append new data in json file
-                        with open("./data.json", "r") as data:
-                                # Reading the old data
-                                data_file = json.load(data)
+        if web == "" or email == "" or password == "":
+            self.alert("warning", "Please fill out the blank.")
+            return
 
-                except FileNotFoundError:
-                        with open("./data.json", "w") as data:
-                                        
-                                # Saving updated data
-                                json.dump(new_data, data, indent=4)
+        # Json Data
+        new_data = {web: {"email": email, "password": password}}
 
-                                self.alert("success", f"{web} Successfully Added.")
+        try:
+            with open("./data.json", "r") as data:
+                data_file = json.load(data)
+        except FileNotFoundError:
+            with open("./data.json", "w") as data:
+                json.dump(new_data, data, indent=4)
+                self.alert("success", f"{web} successfully added.")
+        else:
+            if web not in data_file:
+                self.alert("success", f"{web} successfully added.")
+            else:
+                if password not in data_file[web]["password"] and email not in data_file[web]["email"]:
+                    self.alert("success", f"{web} successfully update.")
+                elif password not in data_file[web]["password"]:
+                    self.alert("success", "Password successfully update.")
+                elif email not in data_file[web]["email"]:
+                    self.alert("success", "Email successfully update.")
 
-                else:
-                        if web not in data_file:
-                                self.alert("success", f"{web} successfully added.")
-                                
-                        else:
-                                if password not in data_file[web]["password"] and email not in data_file[web]["email"]:
-                                        self.alert("success", f"{web} successfully update.")
-                                elif password not in data_file[web]["password"]:
-                                        self.alert("success", "Password successfully update.")
-                                elif email not in data_file[web]["email"]:
-                                        self.alert("success", "Email successfully update.")
-                                else:
-                                        self.alert("success", "No data changed.")
+            data_file.update(new_data)
+            with open("./data.json", "w") as data:
+                json.dump(data_file, data, indent=4)
+        finally:
+            self.webLine.clear()
+            self.passwordLine.clear()
 
-                                
-                        # Updating old data to new data
-                        data_file.update(new_data)
-                        with open("./data.json", "w") as data:
+    # Echo Mode
+    def passwordEcho(self):
+        if self.passwordLine.echoMode() == QLineEdit.Password:
+            self.passwordLine.setEchoMode(QLineEdit.Normal)
+            self.passwordEchoType.setIcon(QtGui.QIcon("img/show.png"))
+        else:
+            self.passwordLine.setEchoMode(QLineEdit.Password)
+            self.passwordEchoType.setIcon(QtGui.QIcon("img/hidden.png"))
 
-                                # Saving updated data
-                                json.dump(data_file, data, indent=4)
-
-                # No matter if succeed the try or fail, do this
-                finally:
-                        # Delete all value of input after adding on database
-                        self.websiteLine.clear()
-                        self.passwordLine.clear()      
-                
-        # Search Button
-        def search(self):
-                web = self.websiteLine.text()
-
-                if web == "":
-                        self.alert("warning", "Please fill out the website field.")
-                        return
-
-                try:
-                        # Append new data in json file
-                        with open("./data.json", "r") as data:
-                                # Reading the old data
-                                data_file = json.load(data)
-
-                except FileNotFoundError:
-                        self.alert("warning", "No data found.")
-
-                else:
-                        if web not in data_file:
-                                self.alert("warning", "Data is not exist.")
-                        
-                        else:
-                                self.emailLine.setText(data_file[web]["email"])
-                                self.passwordLine.setText(data_file[web]["password"])
-                                
-        def passwordEcho(self):
-                if self.passwordLine.echoMode() == QLineEdit.Password:
-                        self.passwordLine.setEchoMode(QLineEdit.Normal)
-                        self.passwordEchoType.setIcon(QtGui.QIcon("img/eye.png"))
-                else:
-                        self.passwordLine.setEchoMode(QLineEdit.Password)
-                        self.passwordEchoType.setIcon(QtGui.QIcon("img/hidden.png"))
-
-        # Alert
-        def alert(self, type, message):
-                if type == "warning":
-                        self.pmAlert.setStyleSheet("color: rgb(255, 70, 70);")
-                        self.pmAlert.setText(message)
-                elif type == "success":
-                        self.pmAlert.setStyleSheet("color: rgb(85, 255, 127);")
-                        self.pmAlert.setText(message)
+    # Alert
+    def alert(self, type, message):
+        if type == "warning":
+            self.pmAlert.setStyleSheet("color: rgb(255, 70, 70);")
+            self.pmAlert.setText(message)
+        elif type == "success":
+            self.pmAlert.setStyleSheet("color: rgb(85, 255, 127);")
+            self.pmAlert.setText(message)
 
 
-# Show Ui
 app = QApplication(sys.argv)
 ui = Ui()
 ui.setFixedSize(570, 420)
