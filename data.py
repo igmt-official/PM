62dc66649f37407c65c365ad0c8abacacc100c74
igmt-official <igmtofficial@gmail.com>
2022-03-24 21:34:46 +0800

    In Progress.

--- data.py
+++
@@ -1,5 +0,0 @@
-class Data():
-    def __init__(self, **kwargs):
-        self.website = kwargs.get('website')
-        self.email = kwargs.get('email')
-        self.password = kwargs.get('password')
